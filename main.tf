9e2497d5276c0b2d8fa5d58727426e3026eeb587
Maciej Wilczyński <maciej.wilczynski@blackscreen.pl>
2022-12-20 11:53:10 +0100

    feat: add module code from infrastructure repository

---
+++ main.tf
@@ -0,0 +1,120 @@
+locals {
+  name       = join("-", compact([var.cluster_name, var.name_suffix]))
+  short_name = substr(local.name, 0, 26) # Shorter name used to bypass 32 char limitation for target groups
+}
+
+resource "aws_lb" "nlb" {
+  name                             = local.name
+  internal                         = var.internal
+  load_balancer_type               = "network"
+  subnets                          = var.public_subnets
+  enable_cross_zone_load_balancing = true
+  enable_deletion_protection       = true
+
+  tags = {
+    "elbv2.k8s.aws/cluster"    = var.cluster_name
+    "service.k8s.aws/resource" = "LoadBalancer"
+    "service.k8s.aws/stack"    = var.application
+  }
+}
+
+resource "aws_lb_listener" "tls" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port              = "443"
+  protocol          = "TLS"
+  certificate_arn   = var.acm_arn
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.tls.arn
+  }
+
+  tags = {
+    "elbv2.k8s.aws/cluster"    = var.cluster_name
+    "service.k8s.aws/resource" = "443"
+    "service.k8s.aws/stack"    = var.application
+  }
+}
+
+resource "aws_lb_listener_certificate" "extra" {
+  count           = length(var.acm_extra_arns)
+  listener_arn    = aws_lb_listener.tls.arn
+  certificate_arn = element(var.acm_extra_arns, count.index)
+}
+
+resource "aws_lb_listener" "plain" {
+  load_balancer_arn = aws_lb.nlb.arn
+  port              = "80"
+  protocol          = "TCP"
+
+  default_action {
+    type             = "forward"
+    target_group_arn = aws_lb_target_group.plain.arn
+  }
+
+  tags = {
+    "elbv2.k8s.aws/cluster"    = var.cluster_name
+    "service.k8s.aws/resource" = "80"
+    "service.k8s.aws/stack"    = var.application
+  }
+}
+
+resource "aws_lb_target_group" "tls" {
+  name     = "${local.short_name}-tls"
+  port     = 60443
+  protocol = "TCP"
+  vpc_id   = var.vpc_id
+
+  target_type = "ip"
+
+  tags = {
+    "elbv2.k8s.aws/cluster"    = var.cluster_name
+    "service.k8s.aws/resource" = "${var.application}:443"
+    "service.k8s.aws/stack"    = var.application
+  }
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 3
+    interval            = 10
+    port                = "traffic-port"
+    protocol            = "TCP"
+    unhealthy_threshold = 3
+  }
+
+  stickiness {
+    cookie_duration = 0
+    enabled         = false
+    type            = "source_ip"
+  }
+}
+
+resource "aws_lb_target_group" "plain" {
+  name     = "${local.short_name}-plain"
+  port     = 60080
+  protocol = "TCP"
+  vpc_id   = var.vpc_id
+
+  target_type = "ip"
+
+  tags = {
+    "elbv2.k8s.aws/cluster"    = var.cluster_name
+    "service.k8s.aws/resource" = "${var.application}:80"
+    "service.k8s.aws/stack"    = var.application
+  }
+
+  health_check {
+    enabled             = true
+    healthy_threshold   = 3
+    interval            = 10
+    port                = "traffic-port"
+    protocol            = "TCP"
+    unhealthy_threshold = 3
+  }
+
+  stickiness {
+    cookie_duration = 0
+    enabled         = false
+    type            = "source_ip"
+  }
+}
---
+++ outputs.tf
@@ -0,0 +1,22 @@
+output "ready" {
+  description = "Hack! Because modules with providers (cluster-apps) cannot use depends_on output value needs to be used to make sure those are provisioned in correct order."
+  value = {
+    tls   = "${aws_lb_listener.tls.arn}:${aws_lb_target_group.tls.id}"
+    plain = "${aws_lb_listener.plain.arn}:${aws_lb_target_group.plain.id}"
+  }
+}
+
+output "arn" {
+  description = "The ARN of the NLB."
+  value       = aws_lb.nlb.arn
+}
+
+output "dns_name" {
+  description = "The DNS name of the NLB."
+  value       = aws_lb.nlb.dns_name
+}
+
+output "zone_id" {
+  description = "The zone ID of the NLB."
+  value       = aws_lb.nlb.zone_id
+}
